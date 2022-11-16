e64a33efb2573b5a74234b69ad9a5113e406a54c
Saadi Ahmad <36916150+saadiahmadx@users.noreply.github.com>
2022-11-15 21:52:50 -0500

    style change + responsive

--- templates.py
+++ templates.py
@@ -1,61 +1,36 @@
 FOOTER_TEMPLATE = ('''
-<hr>
-Created by Nickolas Casalinuovo and Saadi Ahmad.
-<hr>
+
+<center class="footer"> Created by Nickolas Casalinuovo and Saadi Ahmad. </center>
+
 ''')
 
 HEADER_TEMPLATE = ('''
-<h1>Registrar's Office</h1>
+<center><h1 style="background-color:#E77500; color:white" >Registrar's Office: Class Search</h1></center>
 ''')
 
 FORM_TEMPLATE = ('''
-<hr>
+
 <form action="" method="get">
-    <table>
-        <tbody><tr>
-        <td align="right">Dept:</td>
-        <td><input type="text" name="dept" value="{{dept}}"><br></td>
-        </tr>
-
-        <tr>
-        <td align="right">Number:</td>
-        <td>
-            <input type="text" name="coursenum" value="{{coursenum}}"><br>
-        </td>
-        </tr>
-
-        <tr>
-        <td align="right">Area:</td>
-        <td><input type="text" name="area" value="{{area}}"><br></td>
-        </tr>
-
-        <tr>
-        <td align="right">Title:</td>
-        <td>
-            <input type="text" name="title" value="{{title}}"><br>
-        </td>
-        </tr>
-
-        <tr>
-        <td></td>
-        <td><input type="submit"></td>
-        </tr>
-
-    </tbody></table>
+    <div class='search' style="">
+            <input type="text" style="color:white" name="dept" placeholder="Department" value="{{dept}}">
+            <input type="text" name="coursenum" placeholder="Number" value="{{coursenum}}">
+            <input type="text" name="area" placeholder="Area" value="{{area}}">
+            <input type="text" name="title" placeholder="Title" value="{{title}}">
+    </div>
 </form>
-<hr>
+<br>
 ''')
 
 
-SEARCH_RESULTS_TEMPLATE =  ('''
+SEARCH_RESULTS_TEMPLATE = ('''
     <table>
         <tbody>
             <tr>
-                <td><strong>ClassId</strong></td>
-                <td><strong>Dept</strong></td>
-                <td><strong>Num</strong></td>
-                <td><strong>Area</strong></td>
-                <td><strong>Title</strong></td>
+                <th><strong>ClassId</strong></th>
+                <th><strong>Dept</strong></th>
+                <th><strong>Num</strong></th>
+                <th><strong>Area</strong></th>
+                <th><strong>Title</strong></th>
             </tr>
             {{results}}
         </tbody>
@@ -66,12 +41,137 @@ INDEX_TEMPLATE = ('''
     <!DOCTYPE html>
         <html>
         <head>
-            <title>Registrar's Office</title>
+            <title>Registrar's Office Class Search</title>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+            <style>
+                h1 {
+                margin-bottom: 10px;
+                padding-top: 10px;
+                font-family: "Lato";
+                font-size:calc(2vw + 20px);
+                
+                }
+                body {
+                font-family: "Lato";
+                
+                }
+                input         {
+                font-size:20px;
+                margin: 5px;
+                padding:10px 10px 10px 15px;
+
+                border:1px solid #FFFFFF;
+                border-radius: 25px;
+                background-color:#E77500;
+                color:#FFFFFF;
+                min-width: 420px;
+
+                }
+                table {
+                width: 100%;
+                }
+                th {
+                color:#000000;;
+                background:#ffdfbf;
+                //border-bottom:4px solid #9ea7af;
+                font-size:16px;
+                font-weight: 100;
+                padding:10px;
+                text-align:left;
+                text-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+                vertical-align:middle;
+                }
+
+                th:first-child {
+                border-top-left-radius:20px;
+                }
+
+                th:last-child {
+                border-top-right-radius:20px;
+                border-right:none;
+                }
+
+                tr {
+                color:#666B85;
+                font-size:16px;
+                font-weight:normal;
+                text-shadow: 0 1px 1px rgba(256, 256, 256, 0.05);
+                }
+
+                tr:hover td {
+                background:#4E5066;
+                color:#FFFFFF;
+                }
+
+                tr:first-child {
+                border-top:none;
+                }
+
+                tr:last-child {
+                border-bottom:none;
+                }
+
+                tr:nth-child(odd) td {
+                background:#EBEBEB;
+                }
+
+                tr:nth-child(odd):hover td {
+                background:#4E5066;
+                }
+
+                tr:last-child td:first-child {
+                border-bottom-left-radius:3px;
+                }
+
+                tr:last-child td:last-child {
+                border-bottom-right-radius:3px;
+                }
+
+                td {
+                background:#FFFFFF;
+                padding:8px;
+                text-align:left;
+                vertical-align:middle;
+                font-weight:300;
+                font-size:16px;
+                }
+
+                td:last-child {
+                border-right: 0px;
+                }
+                .search {
+                display: flex;
+                flex-wrap: wrap;
+                flex-direction: row;
+                justify-content:center;
+                }
+                ::placeholder {
+                color: #FFFFFF;
+                opacity: 0.6;
+                font-style: italic; 
+                }
+
+                .footer {
+                background-color:#E77500; 
+                color:white;
+                padding: 10px;
+                border-radius: 25px;
+                border-top-left-radius:0px;
+                border-top-right-radius:0px;
+                }
+            
+            </style>
         </head>
         <body>
+        <div style="background-color:#E77500;border-radius: 15px;">
             {{header}}
-            <h2>Class Search</h2>
+            <div style="">
             {{form}}
+            </div>
+            
+        </div>
             {{search_results}}
             {{footer}}
         </body>
@@ -82,18 +182,45 @@ REG_DETAILS_TEMPLATE = ('''
     <!DOCTYPE html>
         <html>
         <head>
-            <title>Registrar's Office</title>
+            <title>Registrar's Office: Class Details</title>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:bold,normal">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <style>
+                h1 {
+                    font-family: "Lato";
+                    font-size:calc(2vw + 20px);
+
+                }
+                body {
+                    font-family: "Lato";
+                    line-height: 1.5rem;
+
+                }
+                .footer {
+                    background-color:#E77500; 
+                    color:white;
+                    padding: 10px;
+                    border-radius: 25px;
+                    border-top-left-radius:0px;
+                    border-top-right-radius:0px;
+                }
+                strong {
+                    font-weight: bold;
+                }
+            </style>
         </head>
         <body>
-            {{header}}
-            <hr>
+        <div style="background-color:#E77500;">
+            <center><h1 style="background-color:#E77500; color:white; padding:40px;" >Registrar's Office: Class Details</h1></center>
+        </div>
+        <div style="padding:0px 10px;">
             <h2>Class Details (class id {{class_id}})</h2>
             <strong>Course Id: </strong>{{course_id}} <br/>
             <strong>Days: </strong>{{days}} <br/>
             <strong>Start time: </strong>{{start_time}} <br/>
             <strong>End time: </strong>{{end_time}} <br/>
             <strong>Building: </strong>{{building}} <br/>
-            <strong>Room: </strong>{{room}} <br/>
+            <strong>Room: </strong>{{room}} <br/> <br/>
 
             <hr>
             <h2>Course Details (course id {{course_id}})</h2>
@@ -103,10 +230,8 @@ REG_DETAILS_TEMPLATE = ('''
             <strong>Title: </strong>{{title}} <br/>
             <strong>Description: </strong>{{description}} <br/>
             <strong>Prerequisites: </strong>{{prerequisites}} <br/>
-            {{professors}}
-
-            <hr>
-            <p> Click here to do <a href="/">another class search</a></p>
+            {{professors}} <br/>
+        </div>
             {{footer}}
         </body>
     </html>
